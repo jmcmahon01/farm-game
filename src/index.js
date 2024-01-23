ed3785536036286562c5d877a00b10ebd8585ed8
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2024-01-23 12:02:45 -0800

    modify import statement

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { Template } from './js/farmgame.js';
-Template();
+
+
+window.onload = function () {
+  document.getElementById('feed').onclick = function () {
+    const newState = stateControl(blueFood);
+    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+  };
+
+  document.getElementById('show-state').onclick = function () {
+    const currentState = stateControl();
+    document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
+  };
+};
+
+import { storeState, stateControl, changeState, feed, blueFood, greenFood, dubiousFood, hydrate, superWater, hydroCannon, itsWhatPlantsCrave } from '../../src/js/farmgame.js'
+
+
+
+
 
 
 
